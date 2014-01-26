d03df4c277ed7e1d3653285fed201c9a3d123bd7
Chris <git.simpkins@gmail.com>
2014-01-26 02:52:03 -0500

    updated help, version bump

--- lib/status/settings.py
+++ lib/status/settings.py
@@ -10,8 +10,8 @@ app_name = 'status'
 # Version Number
 #------------------------------------------------------------------------------
 major_version = "0"
-minor_version = "1"
-patch_version = "1"
+minor_version = "2"
+patch_version = "0"
 
 #------------------------------------------------------------------------------
 # Debug Flag (switch to False for production release code)
@@ -29,17 +29,22 @@ Usage: status [option(s)] <url>
 # Help String
 #------------------------------------------------------------------------------
 help = """
-#------------------------------------------------------------------------------
-# status
-# Copyright 2014 Christopher Simpkins
-# MIT license
-#------------------------------------------------------------------------------
+---------------------------------------
+ status
+ Copyright 2014 Christopher Simpkins
+ MIT license
+---------------------------------------
 
-Status reports the HTTP status code for GET and POST requests from the command line.
+Status reports the HTTP response status code(s) for GET and POST requests.
 
 USAGE
-  status [option(s)] <url>
+  status [-p --post] <url>
 
 OPTIONS
-  -p   --post   POST request (default = GET)
-"""
+  -p  --post    POST request
+
+The URL can be entered with or without the protocol. If you do not enter a protocol (http:// or https://), then http:// is assumed.  If you intend to test with the secure HTTP protocol, then make this explicit in your URL.
+
+EXAMPLES
+  status http://google.com
+  status google.com"""
